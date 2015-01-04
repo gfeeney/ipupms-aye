e287fd1c07dd9c82e8a61009f871fc3302f54bfe
Griffith Feeney <feeney@gfeeney.com>
2015-01-04 18:04:24 -0500

    Work with Armenia and fix function that creates Excel spreadsheet for manual editing of universe information. Jonathan caught the mistake I was making.

--- processing/Rfunctions1/variable.metadata.autocreate.R
+++ processing/Rfunctions1/variable.metadata.autocreate.R
@@ -1,4 +1,4 @@
-variable.metadata.create <- function(vname) {
+variable.metadata.autocreate <- function(vname) {
   getvariable(vname)             # Get variable
   getvariable("serial")          # Get household serial number
   nhh <- length(unique(serial))  # Get number of households in dataset
@@ -10,29 +10,25 @@ variable.metadata.create <- function(vname) {
   }
   length <- max(nchar(get(vname)))       # Get length of codes
   ncodes  <- length(unique(get(vname)))  # Get number of unique codes in data set                           
-  x <- getcodebook(vname)                              # Is there a codebook?
+  x <- getcodebook(vname)                # Is there a codebook?
   if (!is.na(x[1, 1])) {
     cbook <- "Yes"
   } else {
     cbook <- "No"
   }
-  if (cbook == "Yes") {                                # nvals - Number of values in codebook
-    nvals <- dim(x)[1]
-  } else {
-    nvals <- NA
+  nvals  <- NA                           # Initialize nvals, uvcode, mvcode to NA
+  uvcode <- "NA"
+  mvcode <- "NA"
+  if (cbook == "Yes") {                  
+    nvals  <- dim(x)[1]                          # nvals - Number of values in codebook
+    if (is.element("Unknown", x[, "short"])) {
+      mvcode <- x["Unknown" == x[, "short"], ]   # mvcode if "Unknown" (note initialization to NA)
+    }
+    if (is.element("NIU", x[, "short"])) {
+      uvcode <- x["NIU" == x[, "short"], "code"] # uvcode if "NIU" (note initialization to NA)
+    }
   }
-  list(vname=vname, unit=unit, length=length, ncodes=ncodes, cbook=cbook, nvals=nvals)
+  
+  list(vname=vname, unit=unit, length=length, ncodes=ncodes, cbook=cbook, nvals=nvals, mvcode=mvcode, uvcode=uvcode)
 }
 
-
-#   cat <- "tocome"  # Categorical variable    - Manual spreadsheet edit: Overwrite "Yes" or "No"
-#   mvcode <- NA     # Missing value code      - Manual spreadsheet edit: Overwrite code if any
-#   uvcode <- NA     # Undefined value code    - Manual spreadsheet edit: Overwrite code if any
-#   fvar1  <- NA     # Filter variable name1   - Manual spreadsheet edit: Overwrite variable name if any
-#   fvals1 <- NA     # Filter variable values1 - Manual spreadsheet edit: Overwrite filter variable values
-#   fvar2  <- NA     # Filter variable name2   - Manual spreadsheet edit: Overwrite variable name if any
-#   fvals2 <- NA     # Filter variable values2 - Manual spreadsheet edit: Overwrite filter variable values
-#   fvar3  <- NA     # Filter variable name3   - Manual spreadsheet edit: Overwrite variable name if any
-#   fvals3 <- NA     # Filter variable values3 - Manual spreadsheet edit: Overwrite filter variable values
-#   fvar4  <- NA     # Filter variable name4   - Manual spreadsheet edit: Overwrite variable name if any
-#   fvals4 <- NA     # Filter variable values4 - Manual spreadsheet edit: Overwrite filter variable values
