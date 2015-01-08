c9efc7d29f9c3ff7501cc51bcaf08d9ca8774850
Griffith Feeney <feeney@gfeeney.com>
2015-01-08 14:13:54 -0500

    Parity distribution plot script in reasonably final form

--- samples/bolivia2001/parity.distribution.script.R
+++ samples/bolivia2001/parity.distribution.script.R
@@ -19,43 +19,56 @@ agen <- as.numeric(age)
 unv <- sex == "2" & agen >= 15 & agen <= 98
 age.ceb <- table(age[unv], ceb[unv])
 ceb4549 <- age.group(age.ceb, 45:49)
-y2 <- as.vector(ceb4549[1:19])     # Exclude CEB MV
-x <- as.numeric(names(y1))
-y1 <- round(y1/sum(y2), 4)
+y2 <- ceb4549[1:19]     # Exclude CEB MV
+x <- as.numeric(names(y2))
+y1 <- y2/sum(y2)
+
 
 plot(x, y1)  # Basic plot
 plot(x, y1, xlab="Children Ever Born", ylab="Number of Women")  # Add axis labels
 plot(x, y1, xlab="Children Ever Born", ylab="Number of Women", pch=16)  # Change plo mark
 plot(x, y1, xlab="Children Ever Born", ylab="Number of Women", pch=16, cex=1.25)  # Make marks bigger
 lines(x, y1)  # Add connecting lines
-draw.horizontalreferencelines(500*0:4, xlim=c(-1, 20))  # Add horizontal reference lines
-draw.verticalreferencelines(5*0:3, ylim=c(-100, 2500))  # Add vertical reference lines
-yat <- round((500*0:3)/sum(y1), 3)
-axis(4, at=yat, labels=yat)
+
 # With this approach we can't control tick mark positions, so we do it differently
 
 # Plot specifications
-xlab <- "Children Evern Born"
-xlim <- c(0, 16)       # Set limits of horizontal (x) axis
+xlab <- "Numer of Children Ever Born"
+  xlim <- c(0, 15)       # Set limits of horizontal (x) axis
 xaxt <- "n"            # Suppress plotting of x axis
 xatv <- 5*0:3          # Specify tick mark values
 xaxs <- "r"            # Specify padding of plot area
-ylab <- "Number of Women"
-ylim <- c(0, 0.16)
+ylab1 <- "Proportion of Women"
+ylab2 <- "Number of Women"
+ylim <- c(0, 0.15)
 yaxt <- "n"
 yatv <- 0.05*1:3
 yaxs <- "r"
 
 # Build plot step by step
+samplename <- "bolivia2001"
+filename <- paste("cebplot.", samplename, ".pdf", sep="")
+pdf(file=filename, width=5, height=5, pointsize=8)
+par(mar=c(4, 4, 4, 4))  # bottom, left, top, right 
 plot(xlim, ylim, type="n", 
-     xlab=xlab, xaxt=xaxt, xaxs=xaxs,
-     ylab=ylab, yaxt=yaxt, yaxs=yaxs)
+     xlab="", xaxt=xaxt, xaxs=xaxs,
+     ylab="", yaxt=yaxt, yaxs=yaxs)
 draw.verticalreferencelines(5*0:3, ylim)
 draw.horizontalreferencelines(0.05*0:3, xlim)
-axis(1, at=xatv)  # draw bottom x axis
-axis(2, at=yatv)  # draw left y axis
-axis(4, at=yatv)  # draw top x axis
-axis(3, at=xatv, labels=round(xatv * sum(y2), 0))  # draw right y axis
+axis(1, at=xatv)     # Draw bottom x scale
+mtext(xlab, 1, 2.5)  # Label bottom axis
+axis(2, at=yatv)     # Draw left y scale
+mtext(ylab1, 2, 2.5) # Label left axis
+axis(3, at=xatv)     # Draw top x scale (no label)
+axis(4, at=yatv, labels=round(yatv * sum(y2), 0))  # Draw right y scale
+mtext(ylab2, side=4, 2.5)                          # Label right axis
+points(x, y1, pch=16, cex=1.25)  # Plot ceb distribution points
+lines(x, y1)                     # Draw lines
+year <- substr(samplename, nchar(samplename)-3, nchar(samplename))
+country <- substr(samplename, 1, nchar(samplename) - 4)
+substr(country, 1, 1) <- toupper(substr(country, 1, 1))
+text(0.25, 0.005, paste(country, year), adj=0, cex=1.5)
+dev.off()
 
 # Plot points
 points(x, y1, pch=16, cex=1.25)
